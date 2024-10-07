ec93ee582941339a3837ba64321282b040880be3
PriankaDaasPink <priankadas55555@gmail.com>
2024-10-07 15:00:58 +0600

    nav response issue

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,37 +1,74 @@
 import React from "react";
 import "../CSS/App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import { IoSearch } from "react-icons/io5";
+import { Container, Form, Navbar } from "react-bootstrap";
 
-const Navbar = () => {
+const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-gradient-light fixed-top">
-      <div className="container-fluid navContainer">
-        {/* <a className="navbar-brand" href="https://www.esquiretechnology.com/"> */}
-          <img alt="esquireLoge"
-            className="esquireLogo"
+    <Navbar fixed="top" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="https://www.esquiretechnology.com/">
+          <img
             src="http://www.esquiretechnology.com//storage/settings/March2024/yG8aSc4ffvkxLnKF9mLR.png"
+            alt="esquireLogo"
+            className="esquireLogo"
           />
-        {/* </a> */}
+        </Navbar.Brand>
         <h1>KANBAN BOARD</h1>
+        <Form>
+          <div className="search-bar">
+            <IoSearch className="search-icon" />
+            <input
+              type="text"
+              name="search"
+              placeholder="Search Tasks Here"
+              className="search-input"
+              autocomplete="on"
+            />
+          </div>
+        </Form>
+      </Container>
+    </Navbar>
+    // <nav className="navbar navbar-expand-lg navbar-light bg-gradient-light fixed-top">
+    //   <div className="container-fluid navContainer">
+    //     <a className="navbar-brand" href="https://www.esquiretechnology.com/">
+    //       <img
+    //         alt="esquireLoge"
+    //         className="esquireLogo col-md-2"
+    //         src="http://www.esquiretechnology.com//storage/settings/March2024/yG8aSc4ffvkxLnKF9mLR.png"
+    //       />
+    //     </a>
+    //     <button
+    //       className="navbar-toggler"
+    //       type="button"
+    //       data-bs-toggle="collapse"
+    //       data-bs-target="#navbarSupportedContent"
+    //       aria-controls="navbarSupportedContent"
+    //       aria-expanded="true"
+    //       aria-label="Toggle navigation"
+    //     >
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
 
-        <div>
-          <form className="d-flex">
-            <div class="search-bar">
-              <IoSearch className="search-icon" />
-              <input
-                type="text"
-                name="search"
-                placeholder="Search on Prianka's World"
-                class="search-input"
-                autocomplete="on"
-              />
-            </div>
-          </form>
-        </div>
-      </div>
-    </nav>
+    //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    //       <h1>KANBAN BOARD</h1>
+    //       <form className="d-flex">
+    //         <div className="search-bar">
+    //           <IoSearch className="search-icon" />
+    //           <input
+    //             type="text"
+    //             name="search"
+    //             placeholder="Search Tasks Here"
+    //             className="search-input"
+    //             autocomplete="on"
+    //           />
+    //         </div>
+    //       </form>
+    //     </div>
+    //   </div>
+    // </nav>
   );
 };
 
-export default Navbar;
+export default NavBar;
--- src/Data/initial-data.jsx
+++ src/Data/initial-data.jsx
@@ -1,14 +1,41 @@
 const initialData = {
   tasks: {
-    "task-1": { id: "task-1", title: "Task 1", content: "Task 1 Description, Task 1 Description ,Task 1 Description." },
-    "task-2": { id: "task-2", title: "Task 2", content: "Task 2 Description, Task 2 Description,Task 2 Description." },
-    "task-3": { id: "task-3", title: "Task 3", content: "Task 3 Description,Task 3 Description" },
+    "task-1": {
+      id: "task-1",
+      title: "Task 1",
+      content: "Task 1 Description, Task 1 Description ,Task 1 Description.",
+    },
+    "task-2": {
+      id: "task-2",
+      title: "Task 2",
+      content: "Task 2 Description, Task 2 Description,Task 2 Description.",
+    },
+    "task-3": {
+      id: "task-3",
+      title: "Task 3",
+      content: "Task 3 Description,Task 3 Description,Task 3 Description",
+    },
+    "task-4": {
+      id: "task-4",
+      title: "Task 4",
+      content: "Task 4 Description,Task 4 Description,Task 4 Description",
+    },
+    "task-5": {
+      id: "task-5",
+      title: "Task 5",
+      content: "Task 5 Description,Task 5 Description,Task 5 Description",
+    },
+    "task-6": {
+      id: "task-6",
+      title: "Task 6",
+      content: "Task 6 Description,Task 6 Description,Task 6 Description",
+    },
   },
   columns: {
     "column-1": {
       id: "column-1",
       title: "To Do",
-      taskIds: ["task-1", "task-2", "task-3"],
+      taskIds: ["task-1", "task-2", "task-3","task-4", "task-5", "task-6"],
     },
     "column-2": {
       id: "column-2",
@@ -36,7 +63,14 @@ const initialData = {
       taskIds: [],
     },
   },
-  columnOrder: ["column-1", "column-2", "column-3","column-4", "column-5", "column-6"],
+  columnOrder: [
+    "column-1",
+    "column-2",
+    "column-3",
+    "column-4",
+    "column-5",
+    "column-6",
+  ],
 };
 
 export default initialData;
